806f7dae341c659d147faceaaa744f8a133b3813
Stephen Bradley <stephen.a.bradley90@outlook.com>
2019-04-10 12:38:40 +0100

    Pub: full test complete

--- specs/customer_specs.rb
+++ specs/customer_specs.rb
@@ -5,10 +5,22 @@ require_relative('../customer.rb')
 
 class CustomerTest < MiniTest::Test
 
-def setup
+  def setup
+    @customer1 = Customer.new("Helen", 100.0, 35)
+  end
 
-  
-end
+  def test_customer_name
+    assert_equal("Helen", @customer1.name)
+  end
+
+  def test_customer_age
+    assert_equal(35, @customer1.age)
+  end
+
+  def test_customer_wallet
+    assert_equal(100.0, @customer1.wallet)
+  end
+
 
 
 end
